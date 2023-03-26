583bd6be53c845fd3f2bcdfc6ed698149930a65d
Bharath Seshadri <chasethesky79@Bharaths-MBP.attlocal.net>
2023-03-26 15:48:20 -0500

    rendered a basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
-export const App = () => null
+import { AppContainer, CardContainer, ColumnContainer, ColumnTitle } from "./styles"
+
+export const App = () => {
+  return <AppContainer>
+    <ColumnContainer>
+      <ColumnTitle>Todo</ColumnTitle>
+      <CardContainer>First Item</CardContainer>
+      <CardContainer>Second Item</CardContainer>
+      <CardContainer>Third Item</CardContainer>
+    </ColumnContainer>
+  </AppContainer>
+}
